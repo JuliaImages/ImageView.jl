bb71efc127444718e774e75a825ba87bab6a9397
timholy <tim.holy@gmail.com>
2013-05-16 14:10:30 -0500

    Add external.jl to the repository

---
+++ src/external.jl
@@ -0,0 +1,17 @@
+## External-viewer interface
+function imshow(img, range)
+    if ndims(img) == 2 
+        # only makes sense for gray scale images
+        img = imadjustintensity(img, range)
+    end
+    tmp::String = "tmp.ppm"
+    imwrite(img, tmp)
+    cmd = `$imshow_cmd $tmp`
+    spawn(cmd)
+end
+
+imshow(img) = imshow(img, [])
+
+# 'illustrates' fourier transform
+ftshow{T}(A::Array{T,2}) = imshow(log(1+abs(fftshift(A))),[])
+
