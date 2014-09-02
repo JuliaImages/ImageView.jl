3a900fa4f68164ad6fe6a6ea78d4448f42e8c0db
timholy <tim.holy@gmail.com>
2014-09-02 17:07:39 -0500

    Use grayim in statusbar tests

--- test/statusbar.jl
+++ test/statusbar.jl
@@ -1,5 +1,8 @@
-imgdy = Uint8[y for y=0x00:0xff, x=0:100]
-imgdx = Uint8[x for y=1:100, x=0x00:0xff]
+using Images
+import ImageView
+
+imgdy = grayim(Uint8[y for y=0x00:0xff, x=0:100])
+imgdx = grayim(Uint8[x for y=1:100, x=0x00:0xff])
 ImageView.view(imgdy)
 ImageView.view(imgdx)
 
