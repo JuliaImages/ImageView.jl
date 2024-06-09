cbf0b863c09495909ef9de71b4596734f3520262
Jared Wahlstrand <jwahlstrand@gmail.com>
2024-06-09 13:41:09 -0400

    In contrast GUI, convert new contrast limit values to the right type and only update if the new values are different from the old values. (#309)
    Prevents a crash when the contrast bars cross.

--- src/contrast_gui.jl
+++ src/contrast_gui.jl
@@ -98,9 +98,11 @@ function contrast_gui(enabled::Observable{Bool}, hist::Observable, clim::Observa
                 cmaxT = max(cmaxT, cmaxT+Δ)
             end
             mn, mx = minimum(rng), maximum(rng)
-            cmin, cmax = clamp(cminT, mn, mx), clamp(cmaxT, mn, mx)
-            cgui["slider_min"][] = (rng, cmin)
-            cgui["slider_max"][] = (rng, cmax)
+            cmin, cmax = T(clamp(cminT, mn, mx)), T(clamp(cmaxT, mn, mx))
+            if cmin != cgui["slider_min"][] || cmax != cgui["slider_max"][]
+                cgui["slider_min"][] = (rng, cmin)
+                cgui["slider_max"][] = (rng, cmax)
+            end
         end
         # Update the image contrast
         clim[] = CLim(cmin, cmax)
