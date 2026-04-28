c14e8c9fd8b52ae87548dfdd76a8b1d9efdf6b14
Tim Holy <tim.holy@gmail.com>
2026-04-28 03:41:04 -0500

    even more sleep

--- test/newtests.jl
+++ test/newtests.jl
@@ -20,7 +20,7 @@ end
     @test get_gtk_property(frame, :ratio, Float32) == 1.0
     zr[] = (1:20, 9:10)
     @test zr[].currentview.x == 9..10
-    sleep(1.0)  # allow the Gtk event loop to run (macOS is particularly sensitive to this)
+    sleep(2.0)  # allow the Gtk event loop to run (macOS is particularly sensitive to this)
     @test get_gtk_property(frame, :ratio, Float32) ≈ 0.1
     zr[] = (9:10, 1:20)
     sleep(0.5)
